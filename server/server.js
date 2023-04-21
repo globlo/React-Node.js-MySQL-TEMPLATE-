d348c54ba0010cf47a8757936645778bfcc3f8fd
Akihide(Aiden) Takei <49570577+globlo@users.noreply.github.com>
2023-04-21 17:40:50 -0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const mysql = require('mysql');
 const connection = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "akky6223",
+    password: "password",
     database: "soilmoisturesensor",
     connectionLimit: 10
 });
